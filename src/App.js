2e1a02741f98d62b26c3f1c7bd00b04f76774b24
Gizmo675 <41105943+Gizmo675@users.noreply.github.com>
2022-05-18 14:15:27 +0200

    set all context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from './Component/App/Home'
 // Context
 import {AssociateContext} from './Context/AssociateContext'
 import {ProtectedContext} from './Context/Protectedcontext'
+import {AREContext} from './Context/AREContext'
+import {LimitContext} from './Context/LimitContext'
+import {ScalableContext} from './Context/ScalableContext'
 
 function App() {
   
@@ -18,20 +21,43 @@ function App() {
     needProtection: false,
     noNeedProtection: false
   })
+  const [isScalable, setIsScalable] = useState({
+    isScalable: false,
+    isNotScalable: false
+  })
+  const [isUnderLimit, setIsUnderLimit] = useState({
+    isUnderLimit: false,
+    isNotUnderLimit: false
+  })
+  const [isPaidByARE, setIsPaidByARE] = useState({
+    isPaidByARE: false,
+    isNotPaidByARE: false
+  })
 
   const AssociateValue = {associate, setAssociate}
   const ProtectionValue = {needProtection, setNeedProtection}
+  const ScalableValue = {isScalable, setIsScalable}
+  const LimitValue = {isUnderLimit, setIsUnderLimit}
+  const AREValue = {isPaidByARE, setIsPaidByARE}
 
   return (
     <div id="container">
       <div className="App">
         <BrowserRouter>
         <AssociateContext.Provider value={AssociateValue}>
-          <ProtectedContext.Provider value={ProtectionValue}>
+        <ProtectedContext.Provider value={ProtectionValue}>
+        <AREContext.Provider value={AREValue}>
+        <LimitContext.Provider value={LimitValue}>
+        <ScalableContext.Provider value={ScalableValue}>
+            
           <Routes>
             <Route path='/' element={<Home />} />
           </Routes>
-          </ProtectedContext.Provider>
+
+        </ScalableContext.Provider>
+        </LimitContext.Provider>
+        </AREContext.Provider>
+        </ProtectedContext.Provider>
         </AssociateContext.Provider>
         </BrowserRouter>
       </div>
